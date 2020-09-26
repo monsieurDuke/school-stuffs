2750797e635b28f51790940bfd7775ad1078bfe6
hotpotcookie <hotpotcookie@gmail.com>
2020-09-26 08:22:32 +0700

    adding kaka's assignment

---
+++ sql/bsisdata-icang.sql
@@ -0,0 +1,10 @@
+INSERT INTO pekerjaan (NIP,TTL,NAMA,ALAMAT,KOTA,JK)VALUES 
+('12345','12/01/1978','Udin','Jl. Masjid 47','Sleman','P'),
+('12346','19/01/1970','Siti Umami','Jl. Arjuna','Sleman','W'),
+('12347','14/03/1969','Dian','Jl. Arum Dalu 23','Bantuk','P'),
+('12348','13/05/1966','Bagus','Jl. Karang Waru No.13','Magelang','P'),
+('12349','01/02/1975','Intan','Jl. Sawo 108','Yogya','W'),
+('12350','05/04/1971','Edi','Jl. Ngampilan 2A','Sleman','P'),
+('12351','24/04/1969','Sinta','Jl. Kertosono 87','Klaten','W'),
+('12352','04/07/1971','Sueb','Jl Astina 4A','Yogya','P'),
+('12353','05/05/1978','Jonas','Jl Karya Cipta 9','Yogya','P');
